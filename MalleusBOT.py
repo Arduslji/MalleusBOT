13bc2bbc18d5901f52cd6d585211f85b32bd106a
Arduslji <arduslji@gmail.com>
2025-07-20 15:19:06 +0200

    Update MalleusBOT.py

--- MalleusBOT.py
+++ MalleusBOT.py
@@ -349,7 +349,7 @@ async def main():
             port=PORT,
             url_path="webhook", # Importante: SENZA lo slash iniziale qui per url_path di PTB
             webhook_url=WEBHOOK_URL,
-            secret_token=token,
+            secret_token=os.getenv("WEBHOOK_SECRET_TOKEN", "my_secret_token_here"), # Usa una variabile d'ambiente o una stringa predefinita semplice
             allowed_updates=Update.ALL_TYPES,
             drop_pending_updates=True
         )
